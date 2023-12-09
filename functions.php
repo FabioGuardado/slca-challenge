d688ae5bf21b4881c75374e5c8f9dcdb3153904b
Fabio Guardado <fabio.guardado2012@gmail.com>
2023-12-09 14:17:10 -0600

    :lipstick: Add Poppins font

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function slcachallenge_styles(){
   wp_enqueue_style('normalize', get_stylesheet_directory_uri() . '/css/normalize.css');
   wp_enqueue_style('bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
   wp_enqueue_style('style', get_stylesheet_uri());
+  wp_enqueue_style('globals', get_stylesheet_directory_uri() . '/css/globals.css');
   wp_enqueue_script( 'bootstrapjs', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js");
 }
 
