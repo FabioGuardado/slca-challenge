8f0afebe665c0357952ca8fcb21a889e762cedab
Fabio Guardado <fabio.guardado2012@gmail.com>
2023-12-10 21:57:51 -0600

    :sparkles: Add navbar component

---
+++ patterns/navbar-logo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Navbar logo component
+ * Slug: slca-challenge/navbar-logo
+ * Categories: navbar-logo
+ * Block Types: template-part/navbar-logo
+ */
+?>
+
+<img class="slca-logo" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/slca_logo_negativo.svg" alt="Silverline logo"/>
