fbd6d7e396ee1458315bdc125e25f5efb4e8d94e
Fabio Guardado <fabio.guardado2012@gmail.com>
2023-12-10 23:37:07 -0600

    :lipstick: Add flexbox to the video container

--- patterns/header-video.php
+++ patterns/header-video.php
@@ -6,6 +6,6 @@
  * Block Types: template-part/header-video
  */
 ?>
-<div id="videoContainer" data-video="https://www.youtube.com/embed/Dnu2I6DoGzQ?si=r0iG2ZPsS-oKNqA3&autoplay=1">
+<div id="videoContainer" class="header-section__video-container" data-video="https://www.youtube.com/embed/Dnu2I6DoGzQ?si=r0iG2ZPsS-oKNqA3&autoplay=1">
   <img class="header-section__video" id="headerVideo" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/VideoThumbnail_IntroSLCA.webp" />
 </div>
