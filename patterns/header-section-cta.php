408daf3d8ed3964c0cfef6fd56b0081802c78876
Fabio Guardado <fabio.guardado2012@gmail.com>
2023-12-12 16:52:36 -0600

    :sparkles: Redesign header

---
+++ patterns/header-section-cta.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Header CTA component
+ * Slug: slca-challenge/header-cta
+ * Categories: header-cta
+ * Block Types: template-part/header-cta
+ */
+?>
+
+<a class="header-section__cta" href="https://www.linkedin.com/company/silverline-central-america/">Follow Us</a>
+<img class="social-media__icon" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/SocialMedia/linkedin.svg" alt="linkedin"/>
