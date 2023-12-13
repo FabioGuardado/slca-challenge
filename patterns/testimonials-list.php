ef9d6cd3caf5df544f7cd3de1a264ce5b1029694
Fabio Guardado <fabio.guardado2012@gmail.com>
2023-12-12 19:59:45 -0600

    :sparkles: Redesign testimonials section

---
+++ patterns/testimonials-list.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Title: Testimonials List component
+ * Slug: slca-challenge/testimonials-list
+ * Categories: testimonials-list
+ * Block Types: template-part/testimonials-list
+ */
+?>
+
+<div class="row my-4">
+  <div class="col-md-12 testimonial__list">
+    <div class="testimonial__container">
+      <div class="testimonial__header">
+        <img class="testimonial__image" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/gabriela_h.webp" alt="Gabriela H" />
+      </div>
+      <div class="testimonial__body">
+        <p class="testimonial__text">En Silverline Central creen en el talento y apoyan grandemente la superación de cada uno de nosotros. 
+          Me encanta trabajar aquí porque los colaboradores somos valorados por nuestro esfuerzo y los resultados de este mismo, 
+          y porque el ambiente laboral indiscutiblemente es el mejor que puede haber.</p>
+      </div>
+      <div class="testimonial__footer">
+        <p class="testimonial__name"> - Gabriela H</p>
+        <p class="testimonial__position">Junior Developer</p>
+      </div>
+    </div>
+
+    <div class="testimonial__container">
+      <div class="testimonial__header">
+        <img class="testimonial__image" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/carlos_l.webp" alt="Carlos Llort" />
+      </div>
+      <div class="testimonial__body">
+        <p class="testimonial__text">Me siento representado por los valores de la empresa... la exigencia a la mejora continua, 
+          la constante exhortación a seguir aprendiendo, mejorando y certificándote; es impresionante ver con que gusto [entre colaboradores] quien tiene la respuesta al problema, 
+          lo explica y te enseña como resolver.</p>
+      </div>
+      <div class="testimonial__footer">
+        <p class="testimonial__name"> - Carlos Llort</p>
+        <p class="testimonial__position">Junior Developer</p>
+      </div>
+    </div>
+
+    <div class="testimonial__container">
+      <div class="testimonial__header">
+        <img class="testimonial__image" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/napoleon_c.webp" alt="Napoleón Cerón" />
+      </div>
+      <div class="testimonial__body">
+        <p class="testimonial__text">La colaboración que existe entre todos es muy buena, al igual que la cultura y también la innovación. 
+          Tenemos colaboraciones con los equipos de otras regiones. 
+          Las opciones que da Salesforce para trabajar son amplias y en Silverline CA las exploras todas.</p>
+      </div>
+      <div class="testimonial__footer">
+        <p class="testimonial__name"> - Napoleón Cerón</p>
+        <p class="testimonial__position">Junior Developer</p>
+      </div>
+    </div>
+  </div>
+</div>
